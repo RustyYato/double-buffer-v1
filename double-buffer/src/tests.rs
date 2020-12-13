f962da68ce8b2a51f6a881ca0b050812e62839f2
RustyYato <krishna.sd.2012@gmail.com>
2020-12-13 14:17:07 -0500

    add tests

---
+++ double-buffer/src/tests.rs
@@ -0,0 +1,91 @@
+use crossbeam_channel::bounded;
+use crossbeam_utils::thread::scope;
+
+use crate::raw::Buffers;
+
+mod map;
+
+#[test]
+pub fn is_dangling() {
+    let (_, r) = Buffers::new((), ()).split();
+
+    assert!(r.is_dangling());
+}
+
+#[test]
+fn clone_read() {
+    let (_w, r) = Buffers::new((), ()).split();
+
+    r.try_clone().unwrap();
+}
+
+#[test]
+fn write_before_read() {
+    let (mut w, mut r) = Buffers::new(0, 0).split();
+
+    let buffer = &mut *w;
+    *buffer = 20;
+    assert_eq!(*r.get(), 0);
+    w.swap_buffers();
+    assert_eq!(*r.get(), 20);
+    w.swap_buffers();
+    assert_eq!(*r.get(), 0);
+}
+
+#[test]
+#[ignore = "this test will block forever"]
+fn swap_while_read() {
+    let (mut w, mut r) = Buffers::new(0, 0).split();
+
+    let _guard = r.get();
+
+    w.swap_buffers();
+}
+
+#[test]
+#[cfg_attr(miri, ignore)]
+fn wait() {
+    let (mut w, mut r) = Buffers::new(0, 0).split();
+    let r = &mut r;
+
+    let (tx0, rx0) = bounded(1);
+    let (tx1, rx1) = bounded(1);
+    let (tx2, rx2) = bounded(1);
+
+    let _ = scope(move |s| {
+        let _ = s.spawn(move |_| {
+            let _ = tx0.send(());
+
+            let _ = rx1.recv();
+            let _r = r.get();
+            let _ = tx2.send(());
+        });
+
+        let _ = rx0.recv();
+        w.swap_buffers();
+        let _ = tx1.send(());
+        let _ = rx2.recv();
+    });
+}
+
+#[test]
+#[ignore = "this test will block forever"]
+fn blocks() {
+    let (mut w, mut r) = Buffers::new(0, 0).split();
+
+    let (tx0, rx0) = bounded(1);
+    let (tx1, rx1) = bounded(1);
+
+    let _ = tx0.send(&mut r);
+
+    let _ = scope(move |s| {
+        let _ = s.spawn(move |_| {
+            let x = rx0.recv().unwrap();
+            let _ = tx1.send(x.get());
+        });
+
+        let _y = rx1.recv().unwrap();
+
+        w.swap_buffers();
+    });
+}
---
+++ double-buffer/src/tests/map.rs
@@ -0,0 +1,66 @@
+use crate::op::Operation;
+use std::{collections::HashMap, hash::Hash};
+
+pub enum MapOp<K, V> {
+    Insert(K, V),
+    Remove(K),
+    Clear,
+}
+
+impl<K: Clone + Hash + Eq, V: Clone> Operation<HashMap<K, V>> for MapOp<K, V> {
+    fn apply(&mut self, buffer: &mut HashMap<K, V>) {
+        match self {
+            MapOp::Insert(k, v) => {
+                buffer.insert(k.clone(), v.clone());
+            }
+            MapOp::Remove(k) => {
+                buffer.remove(k);
+            }
+            MapOp::Clear => buffer.clear(),
+        }
+    }
+
+    fn into_apply(self, buffer: &mut HashMap<K, V>) {
+        match self {
+            MapOp::Insert(k, v) => {
+                buffer.insert(k, v);
+            }
+            MapOp::Remove(k) => {
+                buffer.remove(&k);
+            }
+            MapOp::Clear => buffer.clear(),
+        }
+    }
+}
+
+#[test]
+fn map_ops() {
+    let (mut w, mut r) = crate::op::new_op_writer();
+
+    w.apply(MapOp::Insert(0, "hello"));
+    w.apply(MapOp::Insert(1, "world"));
+
+    assert_eq!(r.get().len(), 0);
+
+    w.flush();
+
+    w.apply(MapOp::Remove(0));
+    w.apply(MapOp::Insert(2, "!"));
+
+    assert_eq!(r.get().len(), 2);
+    assert_eq!(r.get().get(&0), Some(&"hello"));
+    assert_eq!(r.get().get(&1), Some(&"world"));
+
+    w.flush();
+
+    w.apply(MapOp::Clear);
+
+    assert_eq!(r.get().len(), 2);
+    assert_eq!(r.get().get(&0), None);
+    assert_eq!(r.get().get(&1), Some(&"world"));
+    assert_eq!(r.get().get(&2), Some(&"!"));
+
+    w.flush();
+
+    assert_eq!(r.get().len(), 0);
+}
